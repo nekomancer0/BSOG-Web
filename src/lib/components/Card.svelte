370194cbdc2c34603ef74ccd7815b8b9ef4ae57d
nekomancer0 <nekomancers0@gmail.com>
2024-12-26 18:46:43 +0100

    test
    Co-authored-by: chaptit <chaptit@users.noreply.github.com>

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { BaseThing } from '$lib/types';
+
+	export let card: BaseThing<any>;
+</script>
+
+<div class="card">
+	<img src={card.image} alt={card.name} />
+</div>
+
+<style>
+	.card {
+		cursor: pointer;
+		transition: transform 0.2s ease;
+
+		width: 120px; /* reduced from default size */
+		height: 168px; /* maintaining 1.4:1 aspect ratio */
+
+		&:hover {
+			transform: translateY(-10px) scale(1.1);
+			z-index: 10;
+		}
+
+		img {
+			width: 100%;
+			height: 100%;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,11 @@
+<script>
+	import Card from '$lib/components/Card.svelte';
+	import { Steppes } from '$lib/lands';
+</script>
+
 <div class="container">
 	<h1>Beast: Shapes of Gods</h1>
 	<h2>The Tactical TCG</h2>
-
-	<p>
-		Welcome to the Beast: Shapes of Gods (BSOG) Tactical TCG website! Here you can find information
-		about the game, how to play, and more!
-	</p>
-
-	<p>
-		For more information about the game, check out the <a href="/about">about page</a>!
-	</p>
-
-	<p>
-		For more information about how to play, check out the <a href="/play">play page</a>!
-	</p>
-
-	<p>
-		For more information about the rules, check out the <a href="/rules">rules page</a>!
-	</p>
-
-	<p>
-		For more information about the cards, check out the <a href="/cards">cards page</a>!
-	</p>
-
-	<p>
-		For more information about the expansions, check out the <a href="/expansions"
-			>expansions page</a
-		>!
-	</p>
 </div>
+
+<Card card={Steppes} />
